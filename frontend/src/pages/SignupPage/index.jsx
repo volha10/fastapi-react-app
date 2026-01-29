1ae0081b3a54f900ecc313f59a6c015adda265df
Volha Dziatko <volha10.dziatko@gmail.com>
2026-01-29 20:38:49 +0300

    Add signup form, add sending request to signup endpoint

--- frontend/src/pages/SignupPage/index.jsx
+++ frontend/src/pages/SignupPage/index.jsx
@@ -1,44 +1,101 @@
-import React, { useEffect } from 'react'
+import { useState } from 'react'
 
 
 const SignupPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
 
-  useEffect(() => {
-    const handleSignup = async () => {
-      const mockUser = {
-        name: "User 1",
-        email: "user1@gmail.com",
-        password: "user1password"  
-      };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8000/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData),
+      });
 
-      try {
-        const response = await fetch("http://localhost:8000/signup", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(mockUser),
-        }
+      const result = await response.json();
 
-        );
+      if (response.ok){
+        console.log("Success: ", result);
 
-        const result = await response.json()
-        console.log("Response: ", result)
+      } else {
+        console.log("Error: ", result);
       }
 
-      catch(error) { 
-        console.error("Error: ", error)
-
-      }
+    }
+    catch (error) {
+      console.log("Error: ", error)
     }
 
-    handleSignup();
-  }, []) // [] ensures this only runs once when the component mounts
+  }
+
+  const handleChange = async (e) => {
+    // Dynamically update the state based on input 'name'
+    setFormData({ 
+      ...formData, // 1. Copy all existing data (Spread Operator)
+      [e.target.name]: e.target.value // 2. Update ONLY the field that changed
+    });    
+  }
 
   return (
-    <div>
-        Signup Page
-    </div>
+    <section className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-24 mx-auto flex flex-wrap items-center justify-center">
+        <form onSubmit={handleSubmit} className="lg:w-2/6 md:w-1/2 bg-gray-800 bg-opacity-50 rounded-lg p-8 flex flex-col w-full mt-10 md:mt-0">
+          <h2 className="text-white text-lg font-medium title-font mb-5">Sign Up</h2>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Full Name<span>*</span>
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              required
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email<span>*</span>
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              required
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="password" className="leading-7 text-sm text-gray-400">
+              Password<span>*</span>
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              required
+            />
+          </div>
+          <button type="submit" className="text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">
+            Create account
+          </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
