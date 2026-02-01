947b7c8ea5680dd66716bd61d674ae1c3120f01f
Volha Dziatko <volha10.dziatko@gmail.com>
2026-02-01 15:09:23 +0300

    Add handling of user email already existing error

--- frontend/src/pages/SignupPage/index.jsx
+++ frontend/src/pages/SignupPage/index.jsx
@@ -8,8 +8,12 @@ const SignupPage = () => {
     password: ""
   });
 
+  const [error, setError] = useState("");
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError("");
+
     try {
       const response = await fetch("http://localhost:8000/api/v1/users/signup", {
         method: "POST",
@@ -21,11 +25,12 @@ const SignupPage = () => {
 
       const result = await response.json();
 
-      if (response.ok){
+      if (response.ok) {
         console.log("Success: ", JSON.stringify(result));
 
       } else {
         console.log("Error: ", result);
+        setError(result.detail);
       }
 
     }
@@ -36,11 +41,14 @@ const SignupPage = () => {
   }
 
   const handleChange = async (e) => {
+    if (error) {
+      setError("");
+    }
     // Dynamically update the state based on input 'name'
-    setFormData({ 
+    setFormData({
       ...formData, // 1. Copy all existing data (Spread Operator)
       [e.target.name]: e.target.value // 2. Update ONLY the field that changed
-    });    
+    });
   }
 
   return (
@@ -64,7 +72,7 @@ const SignupPage = () => {
             />
           </div>
           <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+            <label htmlFor="email" className={`leading-7 text-sm text-gray-400 ${error ? "text-red-400" : "text-gray-400"}`}>
               Email<span>*</span>
             </label>
             <input
@@ -74,9 +82,23 @@ const SignupPage = () => {
               name="email"
               value={formData.email}
               onChange={handleChange}
-              className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              className={`w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out ${error ? "border-red-500 focus:ring-red-900" : "border-gray-600 focus:ring-yellow-900"
+                }`}
               required
             />
+
+            {/* Error message with icon */}
+            {error && (
+              <div className="flex items-center gap-1 mt-1.5">
+                <svg xmlns="http://www.w3.org" className="h-3 w-3 text-red-500" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+                <p className="text-red-500 text-[11px] font-medium uppercase tracking-wider">
+                  {error}
+                </p>
+              </div>
+            )}
+
           </div>
           <div className="relative mb-4">
             <label htmlFor="password" className="leading-7 text-sm text-gray-400">
@@ -95,6 +117,9 @@ const SignupPage = () => {
           </div>
           <button type="submit" className="text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">
             Create account
+          </button>
+          <button type="button" className="text-center w-full block text-yellow-500 hover:underline text-xs mt-2">
+            Already have an account? Sign in instead.
           </button>
         </form>
       </div>
